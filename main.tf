4cbe031f48c1457b3eb5aca1e168ff2a0cad0086
Fernando <ffdavis@outlook.com>
2019-04-08 22:00:26 -0400

    vars issues fixed 2

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ module "frontend" {
 
   launch_conf_name             = "TF-LC-Prod"
   auto_scaling_group_name      = "TF-ASG-Prod"
-  instance_tag_name            = "Prod-ASG"
+  instance_tag_name            = "TF-Prod-ASG"
   e_load_balancer_name         = "TF-ELB-Prod"
   instance_security_group_name = "TF-SecG-instance-Prod"
   elb_security_group_name      = "TF-SecG-elb-Prod"

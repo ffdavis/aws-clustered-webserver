6f6078b96172bbbd0e481deb7891b7406d83cf7e
Fernando <ffdavis@outlook.com>
2019-04-08 22:30:59 -0400

    ++ dev-branch.txt creating modules
    correcting main
    
    
    adding variables
    
    
    vars issues fixed
    
    
    vars issues fixed 2

--- modules/vars.tf
+++ modules/vars.tf
@@ -11,10 +11,35 @@
 # ---------------------------------------------------------------------------------------------------------------------
 
 variable "region" {
-	description = "the aws region where we want create the resources"
-	default = "us-east-1"
+  description = "the aws region where we want create the resources"
+  default     = "us-east-1"
 }
+
 variable "server_port" {
   description = "The port the server will use for HTTP requests"
-  default = 8080
+  default     = 8080
+}
+
+variable "launch_conf_name" {
+  description = "Launch Configuration Name"
+}
+
+variable "auto_scaling_group_name" {
+  description = "Auto Scaling Group Name"
+}
+
+variable "instance_tag_name" {
+  description = "Instance Tag Name"
+}
+
+variable "e_load_balancer_name" {
+  description = "Elastic Load Balancer Name"
+}
+
+variable "instance_security_group_name" {
+  description = "Instance Security Group Name"
+}
+
+variable "elb_security_group_name" {
+  description = "ELB Security Group Name"
 }

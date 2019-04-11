fef4ad4c3c64907f5631868d2fa55dbfef497c0a
Fernando <ffdavis@outlook.com>
2019-04-10 22:47:25 -0400

    Added local var env

--- main.tf
+++ main.tf
@@ -4,18 +4,22 @@ provider "aws" {
   profile                 = "default"
 }
 
+locals {
+  env = "PROD"
+}
+
 module "frontendProd" {
   source = "./modules/frontend-app"
 
-  launch_conf_name             = "TF-LC-Prod"
-  auto_scaling_group_name      = "TF-ASG-Prod"
-  instance_tag_name            = "TF-Prod-ASG"
-  e_load_balancer_name         = "TF-ELB-Prod"
-  instance_security_group_name = "TF-SecG-instance-Prod"
-  elb_security_group_name      = "TF-SecG-elb-Prod"
-  asgroup_min_size             = 2
-  asgroup_max_size             = 10
-
+  # launch_conf_name             = "TF-LC-Prod"
+  launch_conf_name               = "TF-LC-${local.env}"
+  auto_scaling_group_name        = "TF-ASG-${local.env}"
+  instance_tag_name              = "TF-Inst-${local.env}-ASG"
+  e_load_balancer_name           = "TF-ELB-${local.env}"
+  instance_security_group_name   = "TF-SecG-instance-${local.env}"
+  elb_security_group_name        = "TF-SecG-elb-${local.env}"
+  asgroup_min_size               = 2
+  asgroup_max_size               = 10
   autoscaling_policy_name        = "TF-scaleout-Prod"
   autoscaling_adjustment_type    = "ChangeInCapacity"
   autoscaling_policy_type        = "SimpleScaling"
